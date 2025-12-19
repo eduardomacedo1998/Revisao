34f2c75a307a60ce38b701cb2320dd86cfcaa553
Eduardo macedo <eduardomacedo1502@gmail.com>
2025-12-19 19:53:34 -0300

    Update routes to enhance user authentication flow and add home page redirection

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserControllers;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,16 +14,6 @@ Route::post('/register', [UserControllers::class, 'register']);
 
 // rotas protegidas por autenticação
 
-function authMiddleware($request, $next)
-{
-    if (!session()->has('user')) {
-        return redirect('/')->with('error', 'Please log in to access this page.');
-    }
-    return $next($request);
-}
-
-Route::middleware('authMiddleware')->group(function () {
-    Route::get('/home', function () {
-        return view('home');
-    })->name('home');
-});
+Route::get('/home', function () { // Rota para a página home após o login bem-sucedido
+    return view('home');
+})->name('home');
